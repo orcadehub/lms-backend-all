d29756eef219015c99b39a2a962581041aef2132
AZZU <orcadehub2@gmail.com>
2026-02-22 16:33:33 +0530

    changes

--- server.js
+++ server.js
@@ -61,8 +61,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key', 'x-tenant-id']
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb', parameterLimit: 50000 }));
 
 // Database connection with serverless optimization
 let cachedConnection = null;
