eaca4ac16e4969d47e96377911e94899d0c9b218
AZZU <orcadehub2@gmail.com>
2026-03-19 20:11:39 +0530

    fix: increased SQL connection timeout

--- config/sqlPlaygroundDb.js
+++ config/sqlPlaygroundDb.js
@@ -7,7 +7,10 @@ const getSqlPool = () => {
   if (!sqlPool) {
     sqlPool = new Pool({
       connectionString: process.env.POSTGRES_URI || 'postgresql://postgres:postgres@localhost:5432/sql_playground',
-      ssl: process.env.POSTGRES_URI ? { rejectUnauthorized: false } : false
+      ssl: process.env.POSTGRES_URI ? { rejectUnauthorized: false } : false,
+      max: 10,
+      idleTimeoutMillis: 30000,
+      connectionTimeoutMillis: 10000, // 10 seconds
     });
   }
   return sqlPool;
