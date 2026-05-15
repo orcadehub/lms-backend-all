4a33a9995b932b411d48d4416b3c396e4d9e68ab
AZZU <orcadehub2@gmail.com>
2026-05-15 22:15:12 +0530

    chore: cleanup unwanted seed files and documentation

--- test_sdk.js
+++
@@ -1,2 +0,0 @@
-const sdk = require('@phonepe-pg/pg-sdk-node');
-console.log(Object.keys(sdk));
--- test_sdk3.js
+++
@@ -1,9 +0,0 @@
-const { StandardCheckoutClient, Env, StandardCheckoutPayRequest } = require('@phonepe-pg/pg-sdk-node');
-const crypto = require('crypto');
-
-const clientId = "M227J0WSRVQHP_2604082155";
-const clientSecret = "MWE5OGRjNzUtOTRlYy00NTQwLWI1NTAtODk3YmZhMDRjZTQx";
-const clientVersion = 1; 
-
-const client = StandardCheckoutClient.getInstance(clientId, clientSecret, clientVersion, Env.SANDBOX);
-client.getOrderStatus("MT1234567").then(res => console.log("Status:", JSON.stringify(res, null, 2))).catch(err => console.log("Error:", err.message));
